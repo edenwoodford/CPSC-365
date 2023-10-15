defcfd8a24d8b788aa7ed939f563f26374ab63e6
Eden Woodford <eden.woodford@gmail.com>
2023-10-15 16:22:08 -0400

    fixed genre handling

--- admin.php
+++ admin.php
@@ -31,51 +31,51 @@ Note: all fields are required except actor names and extra genres <br> <br>
 <label for="year">Release Year:<br></label>
 <input type="text" name="year" required><br>
  <br> Movie Poster: <br>
-<input type="file" name="upload">
+<input type="file" name="upload" accept= ".jpg">
 <label for="genre1"><br>Genre 1:<br></label>
 <select name="genre1" required>
-    <option value="N/A">N/A</option>
-    <option value="Action">Action</option>
-    <option value="Comedy">Comedy</option>
-    <option value="Drama">Drama</option>
-    <option value="Romance">Romance</option>
-    <option value="True Crime">True Crime</option>
-    <option value="True Crime">Sitcom</option>
-    <option value="Sci-Fi">Sci-Fi</option>
-    <option value="Horror">Horror</option>
-	<option value="Mystery">Mystery</option>
-	<option value="Western">Western</option>
-	<option value="Documentary">Documentary</option>
+  <option value="N/A">N/A</option>
+    <option value="1">Action</option>
+    <option value="2">Comedy</option>
+    <option value="3">Drama</option>
+    <option value="4">Romance</option>
+    <option value="5">True Crime</option>
+    <option value="6">Sitcom</option>
+    <option value="7">Sci-Fi</option>
+    <option value="8">Horror</option>
+    <option value="9">Mystery</option>
+    <option value="10">Western</option>
+    <option value="11">Documentary</option>
 </select><br>
 <label for="genre2">Genre 2:<br></label>
 <select name="genre2" required>
-    <option value="N/A">N/A</option>
-    <option value="Action">Action</option>
-    <option value="Comedy">Comedy</option>
-    <option value="Drama">Drama</option>
-    <option value="Romance">Romance</option>
-    <option value="True Crime">True Crime</option>
-    <option value="True Crime">Sitcom</option>
-    <option value="Sci-Fi">Sci-Fi</option>
-    <option value="Horror">Horror</option>
-	<option value="Mystery">Mystery</option>
-	<option value="Western">Western</option>
-	<option value="Documentary">Documentary</option>
+	<option value="N/A">N/A</option>
+    <option value="1">Action</option>
+    <option value="2">Comedy</option>
+    <option value="3">Drama</option>
+    <option value="4">Romance</option>
+    <option value="5">True Crime</option>
+    <option value="6">Sitcom</option>
+    <option value="7">Sci-Fi</option>
+    <option value="8">Horror</option>
+    <option value="9">Mystery</option>
+    <option value="10">Western</option>
+    <option value="11">Documentary</option>
 </select><br>
 <label for="genre3">Genre 3:<br></label>
 <select name="genre3" required>
-    <option value="N/A">N/A</option>
-    <option value="Action">Action</option>
-    <option value="Comedy">Comedy</option>
-    <option value="Drama">Drama</option>
-    <option value="Romance">Romance</option>
-    <option value="True Crime">True Crime</option>
-    <option value="True Crime">Sitcom</option>
-    <option value="Sci-Fi">Sci-Fi</option>
-    <option value="Horror">Horror</option>
-	<option value="Mystery">Mystery</option>
-	<option value="Western">Western</option>
-	<option value="Documentary">Documentary</option>
+  <option value="N/A">N/A</option>
+    <option value="1">Action</option>
+    <option value="2">Comedy</option>
+    <option value="3">Drama</option>
+    <option value="4">Romance</option>
+    <option value="5">True Crime</option>
+    <option value="6">Sitcom</option>
+    <option value="7">Sci-Fi</option>
+    <option value="8">Horror</option>
+    <option value="9">Mystery</option>
+    <option value="10">Western</option>
+    <option value="11">Documentary</option>
 </select><br>
 <label for="director">Director:<br></label>
 <input type="text" name="director" required><br>        
--- doadmin.php
+++ doadmin.php
@@ -18,18 +18,18 @@ if ($checkMovies) {
 }
 
 else {
-	$addMovie = 'INSERT INTO Movies (title, description, year, director, upload) VALUES (:title, :description, :year, :director, :upload)';
+	$addMovie = 'INSERT INTO Movies (title, description, year, director) VALUES (:title, :description, :year, :director)';
 	$stmt = $pdo->prepare($addMovie);
 	$title = $_POST['title'];
 	$description = $_POST['description'];
 	$year= $_POST['year'];
 	$director = $_POST['director'];
-	$upload = $_POST['upload'];
+	//$upload = $_POST['upload'];
     $stmt->bindParam(':title', $title);
     $stmt->bindParam(':description', $description );
     $stmt->bindParam(':year', $year);
     $stmt->bindParam(':director',$director);
-	$stmt->bindParam(':upload', $upload);
+	//$stmt->bindParam(':upload', $upload);
     $stmt->execute();
     $movie_id = $pdo->lastInsertId();
 
@@ -49,19 +49,29 @@ if (isset($_FILES['upload'])) {
 			exit();
 	}else {
         $upload_dir = 'uploads/';
-        $uploaded_file = $upload_dir . $movie_id . '.jpeg';
-        if (move_uploaded_file($_FILES['upload']['tmp_name'], $uploaded_file)) {
+		//name of my folder
+        $filename = $upload_dir . $movie_id . '.jpeg';
+        if (move_uploaded_file($_FILES['upload']['tmp_name'], $filename)) {
             echo 'File uploaded successfully';
         } else {
             echo 'Error moving uploaded file';
             exit();
         }
+	$image = imagecreatefromjpeg ($filename);
+	$width = imagesx ($image);
+	$height = imagesy ($image);
+	$targetWidth = 250;
+	$targetHeight = floor ($height * ($targetWidth/$width));
+	
+	$thumbnail = imagecreatetruecolor ($targetWidth, $targetHeight);
+	imagecopyresampled ($thumbnail, $image, 0, 0, 0, 0, $targetWidth, $targetHeight, $width, $height);
+	imagejpeg ($thumbnail, "movieposter.jpg");
     }
 }
 
 //loop handling a max of 3 genres
 //make sure to check for empty
-for ($i = 1; $i <= 4; $i++) {
+for ($i = 1; $i <= 3; $i++) {
     if (isset($_POST['genre' . $i]) && $_POST['genre' . $i] > 0) {
         $genre_id = $_POST['genre' . $i];
         $addGenre = 'SELECT * FROM Genres WHERE genre_id = :genre_id';
@@ -70,7 +80,8 @@ for ($i = 1; $i <= 4; $i++) {
         $stmt->execute();
         $genreExists = $stmt->fetch();
         if ($genreExists) {
-            $stmt = $pdo->prepare("INSERT INTO MovieGenres (movie_id, genre_id) VALUES (:movie_id, :genre_id)");
+			$linkToMovie = "INSERT INTO MovieGenres (movie_id, genre_id) VALUES (:movie_id, :genre_id)";
+            $stmt = $pdo->prepare($linkToMovie);
             $stmt->bindParam(':movie_id', $movie_id);
             $stmt->bindParam(':genre_id', $genre_id);
             $stmt->execute();
@@ -80,18 +91,20 @@ for ($i = 1; $i <= 4; $i++) {
 //for loop handling up to 3 actors
 for ($i = 1; $i <= 3; $i++) {
   if (!empty($_POST['actor' . $i])) {
-    $addActor = 'INSERT IGNORE INTO Actors (name) VALUES (:name)';
+     $addActor = 'INSERT IGNORE INTO Actors (name) VALUES (:name)';
      $stmt = $pdo->prepare($addActor);
      $stmt->bindParam(':name', $_POST['actor' . $i]);
      $stmt->execute();
      $actor_id = $pdo->lastInsertId();
 if($actor_id == 0) {
-     $stmt = $pdo->prepare("SELECT actor_id FROM Actors WHERE name = :name");
+	 $addNewActors = "SELECT actor_id FROM Actors WHERE name = :name";
+     $stmt = $pdo->prepare($addNewActor);
      $stmt->bindParam(':name', $_POST['actor' . $i]);
      $stmt->execute();
      $actor_id = $stmt->fetch()['actor_id'];
 }
-     $stmt = $pdo->prepare("INSERT INTO SharedMovies (movie_id, actor_id) VALUES (:movie_id, :actor_id)");
+	$sharedMovies = "INSERT INTO SharedMovies (movie_id, actor_id) VALUES (:movie_id, :actor_id)";
+     $stmt = $pdo->prepare($sharedMovies);
      $stmt->bindParam(':movie_id', $movie_id);
      $stmt->bindParam(':actor_id', $actor_id);
      $stmt->execute();
